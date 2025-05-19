bd6f2faaa5bc2bcd7adf0e15ababb4c0020fe9ae
sairam-9999 <sairam4916@gmail.com>
2025-05-19 13:24:34 -0500

    stage 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from './components/header/Header'
 import './App.css'
+import Home from './components/home/Home'
 
 function App() {
 
   return (
     <>
       <Header />
+
+      <main className='main'>
+        <Home></Home>
+      </main>
     </>
   )
 }
