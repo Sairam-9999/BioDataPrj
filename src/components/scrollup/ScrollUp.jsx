3eb4064e4a9ad0d38b0e5c546a0741900da2621e
sairam-9999 <sairam4916@gmail.com>
2025-05-23 12:37:30 -0500

    Stage 11

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -7,7 +7,7 @@ function ScrollUp() {
         const scrollUp = document.querySelector('.scrollup');
         if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
         else scrollUp.classList.remove('show-scroll'); 
-    })
+    });
 
   return (
     <a href="#" className="scrollup">
