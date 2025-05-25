be68835432b673c11f935d3fd7e514fb3d42e55f
sairam-9999 <sairam4916@gmail.com>
2025-05-25 16:46:01 -0500

    Stage 12

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,29 @@
+import work1 from '../../assets/Movie.webp';
+import work2 from '../../assets/Resume.jpg';
+
+export const projectsData = [
+    {
+        id: 1,
+        image: work1,
+        title: 'Movie App',
+        category: 'Web',
+    },
+    {
+        id: 2,
+        image: work2,
+        title: 'Bio-Data',
+        category: 'App',
+    },
+];
+
+export const projectsNav = [
+    {
+        name: 'All',
+    },
+    {
+        name: 'Web',
+    },
+    {
+        name: 'App',
+    },
+];
