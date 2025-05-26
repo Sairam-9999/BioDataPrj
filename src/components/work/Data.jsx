fd310a7b61b42800df9d06049149f767655f8484
sairam-9999 <sairam4916@gmail.com>
2025-05-26 11:44:23 -0500

    Stage 14

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -7,12 +7,14 @@ export const projectsData = [
         image: work1,
         title: 'Movie App',
         category: 'Web',
+        link: "https://github.com/Sairam-9999/prj_lrn/tree/master",
     },
     {
         id: 2,
         image: work2,
         title: 'Bio-Data',
         category: 'App',
+        link: "https://github.com/Sairam-9999/BioDataPrj/tree/master",
     },
 ];
 
--- src/components/work/WorkItem.jsx
+++ src/components/work/WorkItem.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function WorkItem({item}) {
+  
   return (
     <div className="work__card" key={item.id}>
         <img src={item.image} alt=""  className='work__img'/>
         <h3 className="work__title">{item.title}</h3>
-        <a href="#" className="work__button">
+        <a href={item.link} className="work__button" target="_blank" rel="noopener noreferrer" >
             Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
     </div>
